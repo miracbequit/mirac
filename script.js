a8f109e45b4e1fd4861311e1d334cd4e8a8e5b6f
miracbequit <163695114+miracbequit@users.noreply.github.com>
2024-03-17 03:07:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,165 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const title = document.querySelector('.title');
+    const text = title.textContent;
+    title.textContent = '';
+    const possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+
+    for (let i = 0; i < text.length; i++) {
+        const span = document.createElement('span');
+        span.textContent = text[i];
+        span.style.animationDelay = `${i * 0.5}s`; 
+        title.appendChild(span);
+
+        setTimeout(function () {
+            for (let j = 0; j < 10; j++) {
+                setTimeout(function () {
+                    span.textContent = possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+                }, (j * 50));
+            }
+        }, i * 0.5 * 1000);
+
+        setTimeout(function () {
+            span.textContent = text[i];
+        }, (i + 2) * 0.5 * 1000);
+    }
+});
+
+
+
+
+
+
+
+
+
+const heartColors = ['#ff5e5e', '#ffb6b6', '#ff8484', '#ff9292', '#ffaaaa']; 
+const heartInterval = 15; 
+const numberOfHearts = 500; 
+
+// Evet butonu
+const yesButton = document.getElementById('yesButton');
+
+yesButton.addEventListener('click', function () {
+    generateHearts();
+
+
+    const title = document.querySelector('.title');
+    title.textContent = 'Bende biliyordum :)';
+});
+function generateHearts() {
+    const symbols = ['❤️', '💖', '💗', '💕', '💓'];
+    const heartInterval = 100;
+
+    setInterval(() => {
+        const symbol = symbols[Math.floor(Math.random() * symbols.length)]; 
+        createHeart(symbol);
+    }, heartInterval);
+}
+
+function createHeart(symbol) {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.textContent = symbol;
+    heart.style.left = Math.random() * window.innerWidth + 'px';    
+    heart.style.top = Math.random() * window.innerHeight + 'px';             
+    heart.style.animationName = 'heartAnimation';
+    heart.style.animationDuration = '.9s';
+    document.body.appendChild(heart);
+}
+
+const btn = document.getElementById('noButton'); 
+
+let isMoved = false;
+
+btn.addEventListener("mouseover", function() {
+    if (!isMoved) {
+        btn.style.transform = `translate(-80px, 50px)`;
+        isMoved = true; 
+    } else {
+        btn.style.transform = `translate(80px, 50px)`; 
+        isMoved = false;
+    }
+    
+    btn.style.transition = "all 0.3s ease";
+});
+
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Tab') {
+        noButton.style.animation = 'explode 0.5s forwards'; 
+        if (!noButton.dataset.tabPressed) {
+            noButton.dataset.tabPressed = true;
+        setTimeout(() => noButton.style.display = 'none', 500);
+        event.preventDefault(); 
+
+        const title = document.querySelector('.title');
+        title.textContent = 'Bi akıllı sen misin :D kabullen artık';
+        
+
+        setTimeout(() => {
+            title.textContent = 'Seni minnoş şey';
+        }, 2000);
+    }
+}
+});
+
+    document.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    });
+
+
+ 
+    document.onkeydown = function (e) {
+        if (e.ctrlKey && (e.keyCode === 85)) {
+            return false;
+        }
+    }; 
+ 
+    window.addEventListener("DOMContentLoaded", function () {
+        document.addEventListener('keydown', function (e) {
+            if ((e.ctrlKey || e.metaKey) && e.keyCode == 85) {
+                disabledEvent(e);
+            }
+            if ((e.ctrlKey || e.metaKey) && e.keyCode == 76 && e.shiftKey) {
+                disabledEvent(e);
+            }
+        }, false);
+    function disabledEvent(e) {
+            if (e.stopPropagation) {
+        e.stopPropagation();
+            }
+    if (e.preventDefault) {
+        e.preventDefault();
+            }
+    e.cancelBubble = true;
+    e.returnValue = false;
+    return false;
+        }
+    }); 
+
+
+ 
+    window.addEventListener("beforeunload", function (e) {
+        var confirmationMessage = "Sayfayı kapatmak istediğinizden emin misiniz?";
+    e.returnValue = confirmationMessage;
+    return confirmationMessage;
+    });
+    document.addEventListener('keydown', function (e) {
+        if ((e.ctrlKey || e.metaKey) && e.keyCode == 87) {
+        disabledEvent(e);
+        }
+    if (e.altKey && e.keyCode == 115) {
+        disabledEvent(e);
+        }
+    }, false);
+    function disabledEvent(e) {
+        if (e.stopPropagation) {
+        e.stopPropagation();
+        }
+    if (e.preventDefault) {
+        e.preventDefault();
+        }
+    e.cancelBubble = true;
+    e.returnValue = false;
+    return false;
+    } 
